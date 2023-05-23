a60d0589078df9c2649bd348aa11949dcb381778
NataBarsu <khvorostukhina.n@yandex.ru>
2023-05-23 22:46:51 +0300

    Test added

---
+++ src/test/java/PostmanEchTest.java
@@ -0,0 +1,22 @@
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+class PostmanEchoTest {
+
+    @Test
+    void shouldReturnPostedData() {
+        RestAssured.baseURI = "https://postman-echo.com";
+
+        given()
+                .contentType("text/plain; charset=UTF-8")
+                .body("some data")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data"));
+    }
+}    
